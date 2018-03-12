7ec3d716bf4f90bc8167e4cb8d953344691d125f
aussie <saltydoginc@gmail.com>
2018-03-12 21:58:28 +1100

    Update config.cpp

--- mission.pbo/config.cpp
+++ mission.pbo/config.cpp
@@ -30,7 +30,7 @@ class CfgXM8
 		title = "Compass";
 		controlID = 80000;					//IDC:50200 -> 50102 || These need to be unique and out of range from each other
 		logo = "ExAdClient\XM8\Apps\compass\compass.paa";
-        quickFunction = "ExileClientXM8CurrentSlide = 'apps';closeDialog 0;[] execVM 'ExAdClient\XM8\Apps\compass\voyagerCompass.sqf'";
+        	quickFunction = "ExileClientXM8CurrentSlide = 'apps';closeDialog 0;[] execVM 'ExAdClient\XM8\Apps\compass\voyagerCompass.sqf'";
 	};		
 
 }; 
